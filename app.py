2871fd1ad50b93e011608c0e623f294c18959f3d
mhernan10 <mhernan10@outlook.com>
2018-11-12 11:22:03 -0400

    made changes to raml, added more testing scripts

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import sys
+import json
+import ssl
 from flask import Flask, jsonify, abort, request, make_response, session
 from flask_restful import reqparse, Resource, Api
 from flask_session import Session
-import json
 from ldap3 import Server, Connection, ALL
 from ldap3.core.exceptions import *
-import ssl #include ssl libraries
+
 
 import settings # Our server and db settings, stored in settings.py
 
@@ -14,7 +15,7 @@ app = Flask(__name__)
 # Set Server-side session config: Save sessions in the local app directory.
 app.secret_key = settings.SECRET_KEY
 app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SESSION_COOKIE_NAME'] = 'peanutButter'
+app.config['SESSION_COOKIE_NAME'] = 'alpha'
 app.config['SESSION_COOKIE_DOMAIN'] = settings.APP_HOST
 Session(app)
 
@@ -28,21 +29,22 @@ Session(app)
 def not_found(error):
 	return make_response(jsonify( { 'status': 'Bad request' } ), 400)
 
+
 @app.errorhandler(404) # decorators to add to 404 response
 def not_found(error):
 	return make_response(jsonify( { 'status': 'Resource not found' } ), 404)
+
 
 ####################################################################################
 #
 # Routing: GET and POST using Flask-Session
 #
-class SignIn(Resource):
+class Login(Resource):
 	#
 	# Set Session and return Cookie
 	#
 	# Example curl command:
-	# curl -i -H "Content-Type: application/json" -X POST -d '{"username": "Casper", "password": "crap"}'
-	#  	-c cookie-jar -k https://info3103.cs.unb.ca:36371/signin
+	# curl -i -H "Content-Type: application/json" -X POST -d '{"username": "Casper", "password": "crap"}' -c cookie-jar -k https://info3103.cs.unb.ca:36371/signin
 	#
 	def post(self):
 
@@ -87,12 +89,10 @@ class SignIn(Resource):
 	# GET: Check Cookie data with Session data
 	#
 	# Example curl command:
-	# curl -i -H "Content-Type: application/json" -X GET -b cookie-jar
-	#	-k https://info3103.cs.unb.ca:36371/signin
+	# curl -i -H "Content-Type: application/json" -X GET -b cookie-jar -k https://info3103.cs.unb.ca:36371/signin
 	def get(self):
 		success = False
 		if 'username' in session:
-			username = session['username']
 			response = {'status': 'success'}
 			responseCode = 200
 		else:
@@ -107,9 +107,15 @@ class SignIn(Resource):
 	# curl -i -H "Content-Type: application/json" -X DELETE -b cookie-jar
 	#	-k https://info3103.cs.unb.ca:36371/signin
 
-	#
-	#	Here's your chance to shine!
-	#
+	def delete(self):
+		success = False
+		response = { 'status': 'fail' }
+		responseCode = 404
+		if 'username' in session:
+			session.clear()
+			response = { 'status': 'success' }
+			responseCode = 204
+		return make_response(jsonify(response), responseCode)
 
 
 ####################################################################################
@@ -117,7 +123,7 @@ class SignIn(Resource):
 # Identify/create endpoints and endpoint objects
 #
 api = Api(app)
-api.add_resource(SignIn, '/signin')
+api.add_resource(login, '/login')
 
 
 #############################################################################
