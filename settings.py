ff275798464c29edd3e4cbdc125ebcdde0ed566e
mauricio <maurijhn1@gmail.com>
2018-11-11 22:19:06 -0400

    initial commit

---
+++ settings.py
@@ -0,0 +1,14 @@
+#/usr/bin/env python
+# APP_HOST = 'info3103.cs.unb.ca'
+APP_HOST = 'locahost'
+APP_PORT = 36371
+APP_DEBUG = True
+
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'mhernan2'
+MYSQL_PASSWD = 'zZYALS26'
+MYSQL_DB = 'mhernan2'
+
+SECRET_KEY = 'd41d8cd98f00b204e9800998ecf8427e'
+
+LDAP_HOST =  'ldap-student.cs.unb.ca'
