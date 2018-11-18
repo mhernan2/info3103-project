642eae88b8d79dcf923698d559e7e5b3d67f7607
mhernan2 <mhernan2@unb.ca>
2018-11-18 14:36:26 -0400

    modified sql scripts for gifts

--- sql/deleteGift.sql
+++ sql/deleteGift.sql
@@ -1,8 +1,8 @@
 DROP PROCEDURE IF EXISTS deleteGift;
 
 DELIMITER //
-CREATE PROCEDURE deleteGift(IN gift_id_in INT)
+CREATE PROCEDURE deleteGift(IN user_id_in VARCHAR(50), IN gift_id_in INT)
 BEGIN
-DELETE FROM gifts WHERE gift_id=gift_id_in;
+DELETE FROM gifts WHERE from_user=user_id_in AND gift_id=gift_id_in;
 END//
 DELIMITER ;
--- sql/getGiftById.sql
+++ sql/getGiftById.sql
@@ -1,8 +1,8 @@
 DROP PROCEDURE IF EXISTS getGiftById;
 
 DELIMITER //
-CREATE PROCEDURE getGiftById(IN gift_id_in VARCHAR(50))
+CREATE PROCEDURE getGiftById(IN user_id_in VARCHAR(50), IN gift_id_in INT)
 BEGIN
-SELECT * FROM gifts WHERE gift_id=gift_id_in;
+SELECT * FROM gifts WHERE from_user=user_id_in AND gift_id=gift_id_in;
 END//
 DELIMITER ;
